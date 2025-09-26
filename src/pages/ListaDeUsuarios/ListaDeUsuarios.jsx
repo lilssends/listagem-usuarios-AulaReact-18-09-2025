f64eb7b1bc64addaf3a44751c708956240c9692d
Lilssen Kleber <lilssends@gmail.com>
2025-09-25 22:03:19 -0300

    Criando a Integração entr eo Backend e o Frontend

--- src/pages/ListaDeUsuarios/ListaDeUsuarios.jsx
+++ src/pages/ListaDeUsuarios/ListaDeUsuarios.jsx
@@ -1,14 +1,35 @@
-import CardUsuario from '../../components/CardUsuario';
+import { useEffect, useState } from "react";
+import CardUsuario from "../../Components/CardUsuario/CardUsuario";
+import axios from "axios";
 
 function ListaDeUsuarios() {
-    return (
-        <div>
-          <h2>Lista de usuários</h2>
-          <CardUsuario nome="Lilssen Kleber" email="lilssends@gmail.com">  </CardUsuario>
-          <CardUsuario nome="Raquel Mattozo" email="raquelmattozods@gmail.com">  </CardUsuario>
-          <CardUsuario nome="Matheus Mattozo" email="matheusmattozods@gmail.com">  </CardUsuario>
-        </div>
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    BuscarUsuarios();
+  }, []);
+
+  async function BuscarUsuarios() {
+    const response = await axios.get(
+      "https://localhost:44327/api/Usuarios/listar"
     );
+
+    if (response.status != 200) {
+      alert("Erro ao listar usuários");
+      return;
+    }
+    console.log(response);
+    setUsuarios(response.data);
+  }
+
+  return (
+    <div>
+      <h1 style={{ fontFamily: "papyrus", padding: 14, fontWeight: 500, textAlign: "center", color: "cyan", }}> Lista de Usuários</h1>
+      {usuarios.map((usuario) => (
+        <CardUsuario nome={usuario.nome} email={usuario.email}></CardUsuario>
+      ))}
+    </div>
+  );
 }
 
 export default ListaDeUsuarios;
